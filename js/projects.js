5909a5179e8b95ee5f787fcef46c59bc83587c05
daaz-iwnl <dl3185@nyu.edu>
2020-05-31 15:05:26 -0400

    test

--- js/projects.js
+++ js/projects.js
@@ -1,3 +1,3 @@
-$(document).ready(function(e) {
-         alert(projectsList);
-   });
+$.getJSON("https://dyhlstudio.github.io/data/projectslist.json", function(json) {
+    console.log(json);
+});
