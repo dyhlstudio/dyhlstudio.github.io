a202082ec934c363d3309cde6b17da8314fb68e6
daaz-iwnl <dl3185@nyu.edu>
2020-06-04 15:43:55 -0400

    keys

--- js/projects.js
+++ js/projects.js
@@ -1,10 +1,18 @@
 const e = React.createElement;
 
-function propagateProjects() {
-	return e('div',{id: 'entry' + projectsList[1].no}, projectsList[1].title);
-}
+
+var root = projectsList.map(function(){
+	return e('div', {key: projectsList.key}, 
+		e('ul', {className:'project-entry row'}, 
+			e('li', {className:'entry-no d-none d-sm-block col col-sm-2'}, projectsList.no),
+			e('li', {className:'entry-year col col-sm-2'}, projectsList.year),
+			e('li', {className:'entry-title col col-sm-4'}, projectsList.title),
+			e('li', {className:'entry-tags d-none d-md-block col col-sm-4'}, projectsList.tags),
+		)
+	);
+});
 
 ReactDOM.render(
-	propagateProjects(),
-	document.getElementById('react-archive')
+    root,
+    document.getElementById('react-archive')
 );
