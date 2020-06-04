3465cbd530f33ba2dc0f70c1944bc8e34004274f
daaz-iwnl <dl3185@nyu.edu>
2020-06-04 18:13:52 -0400

    progress on project page

--- js/projects.js
+++ js/projects.js
@@ -1,18 +1,29 @@
 const e = React.createElement;
 
+function tagMaker(projectEntry) {
+	var tagsText = ''; 
+	for(i = 0; i < projectEntry.length; i++) {
+		if(i < projectEntry.length - 1) {
+			tagsText += projectEntry[i] + ', ';
+		} else {
+			tagsText += projectEntry[i];
+		};
+	}
+	return tagsText;
+}
 
-var root = projectsList.map(function(){
-	return e('div', {key: projectsList.key}, 
+var bottomList = projectsList.map(function(project){
+	return e('div', {key: parseInt(project.no, 10), id: project.no}, 
 		e('ul', {className:'project-entry row'}, 
-			e('li', {className:'entry-no d-none d-sm-block col col-sm-2'}, projectsList.no),
-			e('li', {className:'entry-year col col-sm-2'}, projectsList.year),
-			e('li', {className:'entry-title col col-sm-4'}, projectsList.title),
-			e('li', {className:'entry-tags d-none d-md-block col col-sm-4'}, projectsList.tags),
+			e('li', {className:'entry-no d-none d-sm-block col col-sm-2'}, project.no),
+			e('li', {className:'entry-year col col-sm-2'}, project.year),
+			e('li', {className:'entry-title col col-sm-4'}, project.title),
+			e('li', {className:'entry-tags d-none d-md-block col col-sm-4'}, tagMaker(project.tags))
 		)
 	);
 });
 
 ReactDOM.render(
-    root,
+    bottomList,
     document.getElementById('react-archive')
 );
