4d273efae4ee16b4cdf28a574df394336ed87bf2
daaz-iwnl <dl3185@nyu.edu>
2020-06-04 14:59:22 -0400

    ahhh

--- data/projectslist.js
+++ data/projectslist.js
@@ -1,7 +1,7 @@
 var projectsList = [
 {
-    "no.": 001,
-    "year": 2020,
+    "no": "001",
+    "year": "2020",
     "title": "Wild Dogs International",
     "logline": "Website for a creative studio in Brooklyn creating site-specific experiences and events",
     "tags": [
@@ -9,8 +9,8 @@ var projectsList = [
     ]
 },
 {
-    "no.": 002,
-    "year": 2020,
+    "no": "002",
+    "year": "2020",
     "title": "Waffle Parking",
     "logline": "Branding and website design for a Los Angeles-based technology startup",
     "tags": [
@@ -19,8 +19,8 @@ var projectsList = [
     ]
 },
 {
-    "no.": 003,
-    "year": 2019,
+    "no": "003",
+    "year": "2019",
     "title": "OPUS Merriweather",
     "logline": "Website concepts for a pioneering arts and music biennial hosted at the Merriweather Post Pavilion in Columbia, MD",
     "tags": [
@@ -28,8 +28,8 @@ var projectsList = [
     ]
 },
 {
-    "no.": 004,
-    "year": 2019,
+    "no": "004",
+    "year": "2019",
     "title": "Psychogeocairn",
     "logline": "Thesis project and interactive installation exploring the shapes and forms of living in spaces.",
     "tags": [
@@ -39,8 +39,8 @@ var projectsList = [
     ]
 },
 {
-    "no.": 005,
-    "year": 2018,
+    "no": "005",
+    "year": "2018",
     "title": "Play Grounds",
     "logline": "Architectural designs for public spaces and playscapes derived from found geography.",
     "tags": [
@@ -49,8 +49,8 @@ var projectsList = [
     ]
 },
 {
-    "no.": 006,
-    "year": 2019,
+    "no": "006",
+    "year": "2019",
     "title": "State of Tyranny",
     "logline": "Exhibition exploring the architecture of control and power at Storefront for Art and Architecture in New York.",
     "tags": [
@@ -59,8 +59,8 @@ var projectsList = [
     ]
 },
 {
-    "no.": 007,
-    "year": 2019,
+    "no": "007",
+    "year": "2019",
     "title": "Aquí vive gente",
     "logline": "Inaugural presentation and exhibition of the Museum of History and Community of Puerta de Tierra",
     "tags": [
@@ -69,8 +69,8 @@ var projectsList = [
     ]
 },
 {
-    "no.": 008,
-    "year": 2019,
+    "no": "008",
+    "year": "2019",
     "title": "Sun Pleats",
     "logline": "Art book showcasing photographs and reflections of a trip I took in Japan.",
     "tags": [
@@ -80,8 +80,8 @@ var projectsList = [
     ]
 },
 {
-    "no.": 009,
-    "year": 2018,
+    "no": "009",
+    "year": "2018",
     "title": "Project Memo",
     "logline": "",
     "tags": [
@@ -91,8 +91,8 @@ var projectsList = [
     ]
 },
 {
-    "no.": 010,
-    "year": 2017,
+    "no": "010",
+    "year": "2017",
     "title": "Draft",
     "logline": "Branding for a fictional, experimental cocktail bar concept.",
     "tags": [
@@ -101,8 +101,8 @@ var projectsList = [
     ]
 },
 {
-    "no.": 011,
-    "year": 2018,
+    "no": "011",
+    "year": "2018",
     "title": "The Architecture House",
     "logline": "Branding for a fictional museum dedicated to architectural study and history",
     "tags": [
@@ -111,8 +111,8 @@ var projectsList = [
     ]
 },
 {
-    "no.": 012,
-    "year": 2017,
+    "no": "012",
+    "year": "2017",
     "title": "Undergrowth",
     "logline": "Short film about dream spaces and the feeling of dreaming.",
     "tags": [
@@ -120,8 +120,8 @@ var projectsList = [
     ]
 },
 {
-    "no.": 013,
-    "year": 2014,
+    "no": "013",
+    "year": "2014",
     "title": "By Omission",
     "logline": "Frag movie showcasing competitive gaming highlights for GuilteTV.",
     "tags": [
@@ -129,8 +129,8 @@ var projectsList = [
     ]
 },
 {
-    "no.": 014,
-    "year": 2015,
+    "no": "014",
+    "year": "2015",
     "title": "Against the Grain",
     "logline": "Custom guitarmaking project from start to finish.",
     "tags": [
--- js/projects.js
+++ js/projects.js
@@ -1,3 +1,10 @@
-$.getJSON("https://dyhlstudio.github.io/data/projectslist.json", function(json) {
-    console.log(json);
-});
+const e = React.createElement;
+
+function propagateProjects() {
+	return e('div',{id: 'entry' + projectsList[1].no}, projectsList[1].title);
+}
+
+ReactDOM.render(
+	propagateProjects(),
+	document.getElementById('react-archive')
+);
