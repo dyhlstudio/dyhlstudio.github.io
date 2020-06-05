8cdc776deebacab31d610e5e635448d33b43da43
daaz-iwnl <dl3185@nyu.edu>
2020-06-05 12:30:11 -0400

    hmm

--- js/images.js
+++ js/images.js
@@ -3,9 +3,19 @@
 $(document).ready(function() {
 	var urls = [];
 	var count = 1;
-	$('active-project').css('background-image', 'url("' + urls[] + '")');
+	$('active-project').css('background-image', 'url("' + urls[0] + '")');
 	setInterval(function(), {
 		$('activeproject').css('background-image', 'url("' + urls[count] + '")');
 		count == urls.length-1 ? count = 0 : count++;
 	}, 5000);
 });
+
+
+var images = createReactClass({
+	getInitialState: function() {
+
+	}
+	render() {
+		return e()
+	}
+}
