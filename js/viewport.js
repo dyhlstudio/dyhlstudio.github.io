32cb36e3ffb46fddb354a847cf68d3a61d132743
daaz-iwnl <dl3185@nyu.edu>
2020-06-16 13:30:14 -0400

    targetdpi

--- js/viewport.js
+++ js/viewport.js
@@ -1,20 +1,7 @@
 window.addEventListener('resize', () => {
     // 1vh of viewport height
+    let vw = window.innerWidth * 0.01;
     let vh = window.innerHeight * 0.01;
-    // Then we set the value in the --vh custom property to the root of the document
-
-    var listCount = $("#numbers > li").length
-    var countHeight = $("#numbers li:first-child").height();
-    var listHeight = listCount * countHeight;
-
-    var headerHeight = $("#archive-categories").height();
-    var botHeight = $("content").height()/2;
-
-    if (botHeight - headerHeight >= listHeight) {
-    	document.documentElement.style.setProperty('--height', `${listHeight}px`);
-    } else {
-        document.documentElement.style.setProperty('--height', `${botHeight-headerHeight}px`);
-    }
+    document.documentElement.style.setProperty('--vh', `${vh}px`);
+    document.documentElement.style.setProperty('--vw', `${vw}px`);
 });
-
-//var(--vh, 1vh) * 30
