3c44a855cd1561fe17f68e8ce075397fcc421f08
daaz-iwnl <dl3185@nyu.edu>
2020-06-18 09:43:29 -0400

    viewport fixes

--- js/viewport.js
+++ js/viewport.js
@@ -1,3 +1,8 @@
+let vw = window.innerWidth * 0.01;
+let vh = window.innerHeight * 0.01;
+document.documentElement.style.setProperty('--vh', `${vh}px`);
+document.documentElement.style.setProperty('--vw', `${vw}px`);
+
 window.addEventListener('resize', () => {
     // 1vh of viewport height
     let vw = window.innerWidth * 0.01;
