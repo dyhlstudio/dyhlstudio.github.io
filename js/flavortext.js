06e38fd3f9618fe55d653f46fd256d98f1568e17
daaz-iwnl <dl3185@nyu.edu>
2020-07-01 11:41:14 -0400

    flavor

--- js/flavortext.js
+++ js/flavortext.js
@@ -1,5 +1,6 @@
 var flavors = [
 	"Pralines, you say!?",
 	"Please enjoy your stay here.",
-	"This site was made partly in React!"
+	"This site was made partly in React!",
+	"&copy; 2020 by Daniel Yunhua Li"
 ];
