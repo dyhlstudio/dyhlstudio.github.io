8740d35a25649a606e36cbcaf83bd45e5a323590
dyhlstudio <hey@dyhl.studio>
2022-04-30 01:43:47 -0400

    flavor

--- data/flavortext.js
+++ data/flavortext.js
@@ -1,7 +1,7 @@
 var flavors = [
 	"Pralines, you say!?",
 	"Please enjoy your stay here.",
-	"This site was made partly in React!",
+	"This site was made partly with React!",
 	"&copy; 2022 by Daniel Yunhua Li",
 	"Pardon my spoon."
 ];
