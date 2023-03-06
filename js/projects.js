6cfeb0244a388a96d0aa2a03f663ff9685408b2d
dyhlstudio <hey@dyhl.studio>
2023-03-05 19:11:57 -0500

    other method

--- js/projects.js
+++ js/projects.js
@@ -99,7 +99,7 @@ $(document).ready(function() {
         document.documentElement.style.setProperty('--sixteennine-thumb', $('.thumb-wrapper').width() * 9 / 16 + 'px');
     });
 
-    $('#nav-4').click(function() {
+    $('#nav-4').on('click tap', function() {
         // close
         if ($('#nav-4').hasClass('on')) {
             $('#nav-4').removeClass('on');
@@ -138,7 +138,7 @@ $(document).ready(function() {
     });
     // Prepare project
 
-    $('.link-wrapper').click(function() {
+    $('.link-wrapper').on('click tap', function() {
         selectionNo = parseInt($(this).attr("id"), 10) - 1;
 
         // toggle project brief
@@ -168,7 +168,7 @@ $(document).ready(function() {
 
         // Open project
 
-        $('.view-links').click(function() {
+        $('.view-links').on('click tap', function() {
             $('body').css('overflow', 'unset');
             onHome = false;
             // determine boundary for setUnsetBoundary
@@ -179,7 +179,7 @@ $(document).ready(function() {
                 $('#home').removeClass('d-flex').addClass('d-none');
             }, 1000);
             // info button
-            $('#info-link').click(function() {
+            $('#info-link').on('click tap', function() {
                 $('body').css('overflow', 'unset');
                 onSlides = false;
                 onInfo = true;
@@ -209,7 +209,7 @@ $(document).ready(function() {
 
     // filtering projects list
 
-    $('#big-text').on('click', '.text-links', function() {
+    $('#big-text').on('click tap', '.text-links', function() {
 
         if ($(this).hasClass('filtered')) {
 
@@ -233,7 +233,7 @@ $(document).ready(function() {
 
     //clear filter on user selecting non-filtered entry
 
-    $('.project-entry').on('click', function() {
+    $('.project-entry').on('click tap', function() {
         if (!($(this).hasClass('filtered'))) {
             $('.project-entry.filtered').removeClass('filtered');
         }
