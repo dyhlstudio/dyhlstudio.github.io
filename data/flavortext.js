109c235d8b6575395ad974bd0a4cbb5e3fcebea6
dyhlstudio <hey@dyhl.studio>
2023-06-22 17:10:36 -0400

    lotus not lily

--- data/flavortext.js
+++ data/flavortext.js
@@ -4,5 +4,6 @@ var flavors = [
 	"This site was made partly with React!",
 	"This site was last updated on 6/21/2023",
 	"Pardon my spoon.",
-	"Click me or refresh the page to see a new font!"
+	"Click me or refresh the page to see a new font!",
+	"🪷"
 ];
