61b47df834efdf02fc4d4327900bfbed18794fd1
dyhlstudio <hey@dyhl.studio>
2023-08-25 02:09:09 -0400

    test

--- js/projects.js
+++ js/projects.js
@@ -442,6 +442,7 @@ function compileProject() {
             $('.threed').attr('shadow-intensity', '1');
             $('.threed').attr('camera-controls', '');
             $('.threed').attr('auto-rotate', '');
+            $('.threed').attr('touch-action', 'pan-y');
         }
     }
 
