f2de74727984bff5c3d95b3359c851c5df62b658
dyhlstudio <hey@dyhl.studio>
2023-08-25 14:22:29 -0400

    what is happening

--- js/projects.js
+++ js/projects.js
@@ -443,7 +443,7 @@ function compileProject() {
             document.querySelector("model-viewer").setAttribute("shadow-intensity", "1");
             document.querySelector("model-viewer").setAttribute("camera-controls", "");
             document.querySelector("model-viewer").setAttribute("auto-rotate", "");
-            document.querySelector("model-viewer").setAttribute("touch-action", "pan-y");
+            //document.querySelector("model-viewer").setAttribute("touch-action", "pan-y");
             console.log('did this work?');
 
         }
